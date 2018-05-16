5937abc8c21e8e60bafdf112d6906c7d7283a7a1
Dei-Program <david.condor@epn.edu.ec>
2018-05-16 11:26:28 -0500

    nestjsDeber_05

--- RutasNestjs/src/app.module.ts
+++ RutasNestjs/src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, NestModule} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {UsuarioController} from "./usuario.controller";
+import {ParametrosController} from "./parametros.controller";
+import {InicioController} from "./inicio.controller";
+import {PreguntasFrecuentesController} from "./preguntasfrecuentes.controller";
+import {LogMiddleware} from "./log.middleware";
+
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ // OtrosModulos
+    ],
+    controllers: [ // Controladores
+        AppController,
+        UsuarioController,
+        ParametrosController,
+        InicioController,
+        PreguntasFrecuentesController
+
+    ],
+    components: [],
 })
-export class AppModule {}
+export class AppModule {
+
+
+}
